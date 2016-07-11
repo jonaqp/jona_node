cbd2943162529582cf1e891573c188502a26659b
Jonathan quiza <jony327@gmail.com>
2016-07-11 03:04:10 -0500

    adding crud

--- client/app/controllers/controllers.js
+++ client/app/controllers/controllers.js
@@ -1,75 +1,93 @@
 angular.module('myApp').controller('loginController',
-  ['$scope', '$location', 'AuthService',
-  function ($scope, $location, AuthService) {
+    ['$scope', '$location', 'AuthService',
+        function ($scope, $location, AuthService) {
 
-    $scope.login = function () {
+            $scope.login = function () {
 
-      // initial values
-      $scope.error = false;
-      $scope.disabled = true;
+                // initial values
+                $scope.error = false;
+                $scope.disabled = true;
 
-      // call login from service
-      AuthService.login($scope.loginForm.username, $scope.loginForm.password)
-        // handle success
-        .then(function () {
-          $location.path('/');
-          $scope.disabled = false;
-          $scope.loginForm = {};
-        })
-        // handle error
-        .catch(function () {
-          $scope.error = true;
-          $scope.errorMessage = "Invalid username and/or password";
-          $scope.disabled = false;
-          $scope.loginForm = {};
-        });
+                // call login from service
+                AuthService.login($scope.loginForm.username, $scope.loginForm.password)
+                // handle success
+                    .then(function () {
+                        $location.path('/');
+                        $scope.disabled = false;
+                        $scope.loginForm = {};
+                    })
+                    // handle error
+                    .catch(function () {
+                        $scope.error = true;
+                        $scope.errorMessage = "Invalid username and/or password";
+                        $scope.disabled = false;
+                        $scope.loginForm = {};
+                    });
 
-    };
+            };
 
-}]);
+        }]);
 
 angular.module('myApp').controller('logoutController',
-  ['$scope', '$location', 'AuthService',
-  function ($scope, $location, AuthService) {
+    ['$scope', '$location', 'AuthService',
+        function ($scope, $location, AuthService) {
 
-    $scope.logout = function () {
+            $scope.logout = function () {
 
-      // call logout from service
-      AuthService.logout()
-        .then(function () {
-          $location.path('/login');
-        });
+                // call logout from service
+                AuthService.logout()
+                    .then(function () {
+                        $location.path('/login');
+                    });
 
-    };
+            };
 
-}]);
+        }]);
 
 angular.module('myApp').controller('registerController',
-  ['$scope', '$location', 'AuthService',
-  function ($scope, $location, AuthService) {
+    ['$scope', '$location', 'AuthService',
+        function ($scope, $location, AuthService) {
 
-    $scope.register = function () {
+            $scope.register = function () {
 
-      // initial values
-      $scope.error = false;
-      $scope.disabled = true;
+                // initial values
+                $scope.error = false;
+                $scope.disabled = true;
 
-      // call register from service
-      AuthService.register($scope.registerForm.username, $scope.registerForm.password)
-        // handle success
-        .then(function () {
-          $location.path('/login');
-          $scope.disabled = false;
-          $scope.registerForm = {};
-        })
-        // handle error
-        .catch(function () {
-          $scope.error = true;
-          $scope.errorMessage = "Something went wrong!";
-          $scope.disabled = false;
-          $scope.registerForm = {};
+                // call register from service
+                AuthService.register($scope.registerForm.username, $scope.registerForm.password)
+                // handle success
+                    .then(function () {
+                        $location.path('/login');
+                        $scope.disabled = false;
+                        $scope.registerForm = {};
+                    })
+                    // handle error
+                    .catch(function () {
+                        $scope.error = true;
+                        $scope.errorMessage = "Something went wrong!";
+                        $scope.disabled = false;
+                        $scope.registerForm = {};
+                    });
+
+            };
+
+        }]);
+
+myApp.controller('navbarController', ['$scope', '$location', 'AuthService',
+    function ($scope, $location, AuthService) {
+        var data = {};
+        var promise = AuthService.getUserStatus();
+
+        promise.then(function (response) {
+            console.log(response);
+            console.log(response.data);
+            if (response.isloggedIn) {
+                $scope.isloggedIn = response.isloggedIn;
+                $scope.username = "Welcome " + response.data.username;
+            }
+            else {
+                $scope.isloggedIn = response.isloggedIn;
+            }
         });
-
-    };
-
-  }]);
+    }]);
